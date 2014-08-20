133cb50c9edd08e3b5c280bf73cc3f4c1fdfb986
Shakil007 <mehedi_nsu07@hotmail.com>
2014-08-20 03:07:08 -0400

    bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mumu::Application.config.secret_token = '24ad28be2bfc54f8e122afae95d557ae57e5eba4204475338e2b0cd560f90293ea3315b7d3c23a8502588681e8a043857028635d95ce9b73dc42426942e5d8bd'
